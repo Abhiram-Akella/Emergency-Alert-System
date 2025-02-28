19621f43ef83adf2262f849b5f1bc43919c45853
Abhiram Akella <abhiram.akella@darwinbox.io>
2025-02-28 12:13:15 +0530

    Added Toasts to display notifications

--- public/javascripts/socket-client.js
+++ public/javascripts/socket-client.js
@@ -2,18 +2,24 @@ const socket = io();
 
 // Listen for reportUpdated events
 socket.on("reportUpdated",(data)=>{
-    alert(data.message);
+    let notifications = JSON.parse(sessionStorage.getItem("notifications")) || [];
+    notifications.push(data.message);
+    sessionStorage.setItem("notifications", JSON.stringify(notifications));
     location.reload();
 });
 
 // Listen for reportAssigned events
 socket.on("reportAssigned",(data)=>{
-    alert(data.message);
+    let notifications = JSON.parse(sessionStorage.getItem("notifications")) || [];
+    notifications.push(data.message);
+    sessionStorage.setItem("notifications", JSON.stringify(notifications));  
     location.reload();
-})
+});
 
 // Listen for reportStatusUpdated event
 socket.on("reportStatusUpdated",(data)=>{
-    alert(data.message);
+    let notifications = JSON.parse(sessionStorage.getItem("notifications")) || [];
+    notifications.push(data.message);
+    sessionStorage.setItem("notifications", JSON.stringify(notifications));   
     location.reload();
-})
+});
